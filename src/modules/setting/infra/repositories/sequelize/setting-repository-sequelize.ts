219bf94380642522cae1af270f0fec0dd199e5c7
Gustavo Bays <bagy@MacBook-Air-de-Bagy.local>
2024-11-04 22:57:49 -0300

    fix: ajustes do getPaginate de setting

--- src/modules/setting/infra/repositories/sequelize/setting-repository-sequelize.ts
+++ src/modules/setting/infra/repositories/sequelize/setting-repository-sequelize.ts
@@ -1,6 +1,8 @@
 import { FindOptions, WhereOptions } from 'sequelize'
 import { injectable } from 'tsyringe'
+import { Meta } from '../../../../../common/contracts/contracts'
 import { SequelizeHelper } from '../../../../../common/db/domain/sequelize/sequelize-helper'
+import { SequelizeRowsToPaginationAdapter } from '../../../../../common/db/infra/adapters/sequelize/sequelize-rows-to-pagination-adapter'
 import { isNotEmpty } from '../../../../../common/helpers/helper'
 import { Setting as ISetting, SettingFilter } from '../../../domain/setting'
 import { SettingRepository } from '../../../domain/setting-repository'
@@ -16,6 +18,10 @@ export class SettingRepositorySequelize implements SettingRepository {
     async getPaginate(filter: SettingFilter): Promise<Meta<ISetting>> {
         const query = this.interpret(filter)
         SequelizeHelper.setPaginationOn(query, filter)
+
+        const { rows, count } = await Setting.findAndCountAll(query)
+
+        return new SequelizeRowsToPaginationAdapter(rows, query, count).getData()
     }
 
     async getOne(filter: SettingFilter): Promise<ISetting> {
